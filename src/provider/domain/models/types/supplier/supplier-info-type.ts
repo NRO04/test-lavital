495882db0cc3a8ce8125d99cb46df9d691b07fbc
RO <ronicolas04@gmail.com>
2021-11-07 16:24:35 -0500

    Redux configuration implemented

---
+++ src/provider/domain/models/types/supplier/supplier-info-type.ts
@@ -0,0 +1,6 @@
+import RegisterSupplierRepository from "../../../repository/register-supplier-repository";
+
+export type SupplierInfo = {
+
+    [supplier_name: string]: RegisterSupplierRepository;
+}
